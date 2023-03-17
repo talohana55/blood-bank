0040251259057f7bc4c13264f4d397c3d9b0ec54
Tal ohana <68163421+talohana55@users.noreply.github.com>
2023-03-17 16:32:31 +0200

    backend update + api

--- controllers/donorController.js
+++ controllers/donorController.js
@@ -1,63 +1,83 @@
-const Donor = require('../models/donor-model');
-
-// Create a new donor
-exports.createDonor = async (req, res) => {
-    try {
-        const donor = new Donor({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            gender: req.body.gender,
-            bloodType: req.body.bloodType,
-            birthDate: req.body.birthDate,
-            contactNumber: req.body.contactNumber,
-            email: req.body.email,
-            address: req.body.address,
-            donations: req.body.donations
-        });
-        const savedDonor = await donor.save();
-        res.status(201).json(savedDonor);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-};
+const Donor = require("../models/donor-model");
 
 // Get all donors
 exports.getAllDonors = async (req, res) => {
-    try {
-        const donors = await Donor.find();
-        res.json(donors);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+  try {
+    const donors = await Donor.find();
+    if (donors.length) {
+      res.status(200).json(donors);
+    } else {
+      res.status(200).json([]);
     }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 // Get a single donor
 exports.getDonor = async (req, res) => {
-    try {
-        const donor = await Donor.findById(req.params.cid);
-        if (!donor) throw Error('Donor not found');
-        res.json(donor);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
+  try {
+    const { id } = req.body;
+    const donor = await Donor.findOne({ cid: id });
+    if (donor) {
+      res.status(200).json(donor);
+    } else {
+      res.status(200).json();
     }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// Create a new donor
+exports.createDonor = async (req, res) => {
+  try {
+    var objectId = new ObjectId();
+    const donor = new Donor({
+      cid: objectId,
+      birthDate: new Date(req.body.birthDate),
+      ...req.body,
+    });
+    await donor.save();
+    res.status(201).json(donor);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 // Update a donor
 exports.updateDonor = async (req, res) => {
-    try {
-        const donor = await Donor.findByIdAndUpdate(req.params.cid, req.body, { new: true });
-        res.json(donor);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
+  try {
+    const donor = await Donor.findOneAndUpdate(
+      { cid: req.body.cid },
+      { ...req.body },
+      { new: true }
+    );
+    if (donor) {
+      console.log("OK!");
+      res.status(200).json("Donor updated successfully!");
+    } else {
+      console.log("problem");
+      res.status(404).json({ message: err.message });
     }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 // Delete a donor
 exports.deleteDonor = async (req, res) => {
-    try {
-        const donor = await Donor.findByIdAndDelete(req.params.cid);
-        res.json(donor);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
+  try {
+    const { cid } = req.body;
+    const donor = await Donor.deleteOne({ cid: cid });
+    if (donor.deletedCount === 1) {
+      console.log("OK!");
+      res.status(200).json("Donor removed successfully!");
+    } else {
+      console.log("problem");
+      res.status(404).json({ message: err.message });
     }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
